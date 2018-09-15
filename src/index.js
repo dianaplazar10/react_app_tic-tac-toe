e28848bd20a8ff71be2fd7b34c35a2eba6df9135
diana <diana@172.21.36.246>
2018-09-15 14:11:08 -0700

    Code updated for complete tic tac toe

--- src/index.js
+++ src/index.js
@@ -2,23 +2,91 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+//class Square extends React.Component {
+////   constructor(props) {
+////       super(props);
+////       this.state = {
+////         value: null,
+////       };
+////   }
+//
+//  render() {
+//    return (
+//     <button className="square" onClick={() => this.props.onClick()} >
+////     onClick={() => this.setState({value:'X'})} >
+////        {this.state.value}
+//        {props.value}
+//      </button>
+//    );
+//  }
+//}
+
+//Replacing the above Square Component/Class with below function
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
 }
 
 class Board extends React.Component {
+   constructor(props) {
+           super(props);
+           this.state = { squares: Array(9).fill(null),
+                          xIsNext: true,
+                        };
+   }
+
+   handleClick(i) {
+       const squares = this.state.squares.slice();
+       if (calculateWinner(squares) || squares[i]) {//this loop stops further clicks if already a winner
+             return;
+           }
+       squares[i] = this.state.xIsNext ? 'X' : 'O';
+       this.setState({
+         squares: squares,
+         xIsNext: !this.state.xIsNext,
+       });
+   }
+
   renderSquare(i) {
-    return <Square />;
+    //return <Square />;//Update this line to dynamically insert data in Square component.
+    //return<Square value={i} />;//In order to make this work.. you will have to update Square component's render() return.
+    return(<Square value={this.state.squares[i]} onClick= { () => this.handleClick(i)} /> );
   }
 
   render() {
-    const status = 'Next player: X';
+//    const status = 'Next player: X';
+//    const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+
+  const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
     return (
       <div>
@@ -110,14 +178,6 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
-//ReactDOM.render(
-//  <Game />,
-//  document.getElementById('root2')
-//);
+ReactDOM.render(<Game />,document.getElementById('root'));
 
 //ReactDOM.render(<NavComponent />,  document.getElementById('navbar'));//document.querySelector('navbar'));
